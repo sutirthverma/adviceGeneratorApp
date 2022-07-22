acc0333bd9cd2944820c7d504464bf2f8e675e6d
Sutirth Verma <86179480+sutirthverma@users.noreply.github.com>
2022-07-22 21:50:42 +0530

    loading sign added

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,48 @@
-const dice = document.querySelector('.diceColumn');
-const adviceID = document.querySelector('.adviceID')
-const advice = document.querySelector('.advice')
+const dice = document.querySelector('.dice');
+const adviceID = document.querySelector('.adviceID');
+const advice = document.querySelector('.advice');
+const loadingCircle = document.querySelector('.loadingCircle');
 
 
-dice.addEventListener('click' , ()=>{
-    fetch("https://api.adviceslip.com/advice")
+const hide = document.querySelector('.advice');
+const show = document.querySelector('.advice');
+
+
+
+fetch("https://api.adviceslip.com/advice")
     .then(response => response.json())
     .then(data => {
-    adviceID.innerHTML = `#${data["slip"].id}`;
-    advice.innerHTML = `"${data["slip"].advice}"`;        
+        //Onload show loading sign
+        setInterval(() => {
+            loadingCircle.classList.add('hide')
+        }, 1000);
+        //After 1000ms show advice
+        setTimeout(() => {
+            adviceID.innerHTML = `#${data["slip"].id}`;
+            advice.innerHTML = `"${data["slip"].advice}"`;
+        }, 1000);
+        //After showing advice increase the size of dice once
+        setTimeout(() => {
+            dice.classList.add('diceAnim')            
+        }, 1200);
     })
+
+
+
+
+//Onclicking dice show another advice
+dice.addEventListener('click', () => {
+    advice.style.display = "none";
+    loadingCircle.classList.replace('hide', 'show')
+    setTimeout(() => {
+        fetch("https://api.adviceslip.com/advice")
+            .then(response => response.json())
+            .then(data => {
+                loadingCircle.classList.replace('show', 'hide')
+                adviceID.innerHTML = `#${data["slip"].id}`;
+                advice.style.display = "block";
+                advice.innerHTML = `"${data["slip"].advice}"`;
+            })
+    }, 1000);
+
 })
